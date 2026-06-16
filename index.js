9ad05cad0174071004a2c28111f3b8a067913a2b
alekey_0809 <gosalavida2017@gmail.com>
2026-06-16 17:57:28 -0500

    coneccion backend con base de datos

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 import express from "express";
+import {config} from "dotenv";
+import pg from "pg";
+
+
+config();
+
+const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+})
 
 const app = express();
 app.get('/',(req,res ) => {
-  res.send('Hello, World!');
+res.send('Hello, World!');
+});
+app.get('/ping',async (req,res ) => {
+    const result= await pool.query('SELECT now()');
+    return res.json(result.rows[0]);
 });
 
 app.listen(3000)
